daefa4927ce5447ea82ab3c3e8735706f763cf88
majudhu <majudhu.sup@gmail.com>
2019-10-25 00:01:58 -0700

    keep only the short req(...) style

--- main.js
+++ main.js
@@ -1,67 +1,6 @@
 'use strict';
 
 async function req(arg1, arg2, arg3) {
-    // req('path')
-    // req('method', 'path')
-    // req('path', {data})
-    // req('method', 'path', {data})
-    let method, path, data;
-    if (typeof arg2 === 'undefined') {
-        path = arg1;
-    } else if (typeof arg3 === 'undefined') {
-        if (typeof arg2 === 'string') {
-            method = arg1;
-            path = arg2;
-        } else {
-            method = 'POST';
-            path = arg1;
-            data = JSON.stringify(arg2);
-        }
-    } else {
-        method = arg1;
-        path = arg2;
-        data = JSON.stringify(arg3);
-    }
-    const res = await fetch(path, {
-        mode: 'same-origin',
-        method: method,
-        headers: { 'Content-Type': 'application/json' },
-        body: data
-    });
-    return { status: res.status, res: await res.json() };
-}
-
-async function req(arg1, arg2, arg3) {
-    // req('path')
-    // req('method', 'path')
-    // req('path', {data})
-    // req('method', 'path', {data})
-    let method, path, data, res;
-    if (typeof arg2 === 'undefined') {
-        res = await fetch(arg1);
-    } else if (typeof arg3 === 'undefined') {
-        if (typeof arg2 === 'string') {
-            res = await fetch(arg2, { method: arg1 })
-        } else {
-            res = await fetch(arg1, {
-                mode: 'same-origin',
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(arg2)
-            });
-        }
-    } else {
-        res = await fetch(arg2, {
-            mode: 'same-origin',
-            method: arg1,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(arg3)
-        });
-    }
-    return { status: res.status, res: await res.json() };
-}
-
-async function req(arg1, arg2, arg3) {
     //     // req('path') GET
     //     // req('method', 'path') arg1
     //     // req('path', {data}) POST
